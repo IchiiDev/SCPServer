a21ec4a4b9beb4068866858f9bb38155f77be5d9
Ichii <ethan.thierry06@gmail.com>
2020-04-23 13:49:29 +0200

    Updated PHP script
    Added new endpoint get_user & renamed endpoint get to get_report

--- scpserver.php
+++ scpserver.php
@@ -61,7 +61,7 @@ function endpoints($endpoint) {
             else return ["error" => 403, "err_message" => "Access Forbidden: Unknown User", "server_version" => SERVER_VERSION];
     
         }
-        elseif ($endpoint == "get") {
+        elseif ($endpoint == "get_report") {
             $get_data = ["token" => get_parameter('token'), "file_name" => get_parameter('file_name')];
             if ($get_data["token"] == null) return ["error" => 400, "err_message" => "User token is not defined", "server_version" => SERVER_VERSION];
             if ($get_data["file_name"] == null) return ["error" => 400, "err_message" => "File name is not defined", "server_version" => SERVER_VERSION];
@@ -83,6 +83,33 @@ function endpoints($endpoint) {
                         }
                     }
                     else return ["error" => 404, "err_message" => "Unknown Report", "server_version" => SERVER_VERSION];
+                }
+            }
+            else return ["error" => 403, "err_message" => "Access Forbidden: Unknown Token", "server_version" => SERVER_VERSION];
+        }
+        elseif ($endpoint == "get_user") {
+            $get_data = ["token" => get_parameter('token'), "username" => get_parameter('username')];
+            if ($get_data["token"] == null) return ["error" => 400, "err_message" => "User token is not defined", "server_version" => SERVER_VERSION];
+            if ($get_data["token"] == null) return ["error" => 400, "err_message" => "Username is not defined", "server_version" => SERVER_VERSION];
+
+            $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+            if ($conn->connect_error) return ["error" => 500, "err_message" => "Server Side Error: Database access forbidden", "server_version" => SERVER_VERSION];
+
+            $token = $get_data["token"];
+            $username = $get_data["username"];
+
+            $result = $conn->query("SELECT permission FROM users WHERE `token`=\"$token\"");
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $row = $row["permission"];
+                    $result = $conn->query("SELECT username, full_name, rank, permission FROM users WHERE `username`=\"$username\"");
+                    if ($result->num_rows > 0) {
+                        while ($row2 = $result->fetch_assoc()) {
+                            if (!($row >= $row2["permission"])) return ["error" => 403, "err_message" => "Access Forbidden: Insufficient Permissions", "server_version" => SERVER_VERSION];
+                            return ["request" => "success", "report_data" => $row2, "server_version" => SERVER_VERSION];
+                        }
+                    }
+                    else return ["error" => 404, "err_message" => "Unknown User", "server_version" => SERVER_VERSION];
                 }
             }
             else return ["error" => 403, "err_message" => "Access Forbidden: Unknown Token", "server_version" => SERVER_VERSION];
