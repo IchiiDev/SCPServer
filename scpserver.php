8249aacb9f16f91436948a622d9d8ad34fa88e32
Ichii <ethan.thierry06@gmail.com>
2020-04-23 19:43:41 +0200

    Security Update
    Added some SQL securities to prevent some security breaches (Thx iTrooz_)

--- scpserver.php
+++ scpserver.php
@@ -9,18 +9,18 @@
 define("STATUS", "public");
 
 # Databases constants
-define("DB_HOST", "host"); # MySQL database hostname (localhost for local database)
-define("DB_USER", "username"); # MySQL Database username 
-define("DB_NAME", "dbname"); # MySQL Schema name
-define("DB_PASSWORD", "password"); # MySQL User password
+define("DB_HOST", "host");
+define("DB_USER", "username");
+define("DB_NAME", "db_name");
+define("DB_PASSWORD", "password");
 
 # URL to send with every file links
-define("URL", "https://yourwebserver.net/"); # Must End with a slash / and start by either https:// or http://
+define("URL", "https://myhost.com/"); # Must End with a slash / and start by either https:// or http://
     
 /*  Program
     Constants   */
-    
-define("SERVER_VERSION", "SCPServer.php[1.0-Beta]");
+ 
+define("SERVER_VERSION", "SCPServer.php[1.1-Beta]");
 define("REQUIRED_CLIENT", ["warn" => "SCPTerminal1.0-b-0", "required" => "SCPTerminal1.0-b-0"]);
 
 /*  Program 
@@ -48,10 +48,16 @@ function endpoints($endpoint) {
             if ($conn->connect_error) return ["error" => 500, "err_message" => "Server Side Error: Database access forbidden", "server_version" => SERVER_VERSION];
     
             $hash = hash("sha256", $auth_data["password"], false);
-            $auth = $auth_data["auth"];
     
-            $result = $conn->query("SELECT * FROM users WHERE `username`=\"$auth\"");
-    
+            $result = $conn->prepare("SELECT * FROM users WHERE `username`=?");
+            $result->bind_param("s", $username);
+
+            $username = $auth_data["auth"];
+            
+            $result->execute();
+
+            $result = $result->get_result();
+
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                     if ($row["password"] != $hash) return ["error" => 403, "err_message" => "Access Forbidden: Invalid Password"];
@@ -69,13 +75,18 @@ function endpoints($endpoint) {
             $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
             if ($conn->connect_error) return ["error" => 500, "err_message" => "Server Side Error: Database access forbidden", "server_version" => SERVER_VERSION];
 
+            $result = $conn->prepare("SELECT permission FROM users WHERE `token`=?");
+            $result->bind_param("s", $token);
             $token = $get_data["token"];
-            $file_name = $get_data["file_name"];
-
-            $result = $conn->query("SELECT permission FROM users WHERE `token`=\"$token\"");
+            $result->execute();
+            $result = $result->get_result();
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    $result = $conn->query("SELECT * FROM reports WHERE name=\"$file_name\"");
+                    $result = $conn->prepare("SELECT * FROM reports WHERE name=?");
+                    $result->bind_param("s", $filename);
+                    $filename = $get_data["file_name"];
+                    $result->execute();
+                    $result = $result->get_result();
                     if ($result->num_rows > 0) {
                         while ($row2 = $result->fetch_assoc()) {
                             if (!($row >= $row2["level"])) return ["error" => 403, "err_message" => "Access Forbidden: Insufficient Permissions", "server_version" => SERVER_VERSION];
@@ -95,14 +106,19 @@ function endpoints($endpoint) {
             $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
             if ($conn->connect_error) return ["error" => 500, "err_message" => "Server Side Error: Database access forbidden", "server_version" => SERVER_VERSION];
 
+            $result = $conn->prepare("SELECT permission FROM users WHERE `token`=?");
+            $result->bind_param("s", $token);
             $token = $get_data["token"];
-            $username = $get_data["username"];
-
-            $result = $conn->query("SELECT permission FROM users WHERE `token`=\"$token\"");
+            $result->execute();
+            $result = $result->get_result();
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     $row = $row["permission"];
-                    $result = $conn->query("SELECT username, full_name, rank, permission FROM users WHERE `username`=\"$username\"");
+                    $result = $conn->prepare("SELECT username, full_name, rank, permission FROM users WHERE `username`=?");
+                    $result->bind_param("s", $username);
+                    $username = $get_data["username"];
+                    $result->execute();
+                    $result = $result->get_result();
                     if ($result->num_rows > 0) {
                         while ($row2 = $result->fetch_assoc()) {
                             if (!($row >= $row2["permission"])) return ["error" => 403, "err_message" => "Access Forbidden: Insufficient Permissions", "server_version" => SERVER_VERSION];
@@ -125,7 +141,11 @@ function endpoints($endpoint) {
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     $row = $row["permission"];
-                    $result = $conn->query("SELECT `name` FROM `reports` WHERE `level` <= $row");
+                    $result = $conn->prepare("SELECT `name` FROM `reports` WHERE `level` <= ?");
+                    $result->bind_param("s", $row2);
+                    $row2 = $row;
+                    $result->execute();
+                    $result = $result->get_result();
                     if ($result->num_rows > 0) {
                         $reports_array = array();
                         while ($row = $result->fetch_assoc()) array_push($reports_array, $row["name"]);
@@ -143,11 +163,19 @@ function endpoints($endpoint) {
             $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
             if ($conn->connect_error) return ["error" => 500, "err_message" => "Server Side Error: Database access forbidden", "server_version" => SERVER_VERSION];
 
-            $result = $conn->query("SELECT permission FROM users WHERE `token`=\"$token\"");
+            $result = $conn->prepare("SELECT permission FROM users WHERE `token`=?");
+            $result->bind_param("s", $token1);
+            $token1 = $token;
+            $result->execute();
+            $result = $result->get_result();
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     $row = $row["permission"];
-                    $result = $conn->query("SELECT `username`, `permission`, `full_name`, `rank` FROM `users` WHERE `permission` <= $row");
+                    $result = $conn->prepare("SELECT `username`, `permission`, `full_name`, `rank` FROM `users` WHERE `permission` <= ?");
+                    $result->bind_param("s", $row2);
+                    $row2 = $row;
+                    $result->execute();
+                    $result = $result->get_result();
                     if ($result->num_rows > 0) {
                         $users_array = array();
                         while ($row = $result->fetch_assoc()) array_push($users_array, $row);
@@ -168,6 +196,5 @@ $data = endpoints(get_parameter('endpoint'));
 
 header('Content-Type: application/json');
 echo json_encode($data);
-
 die();
 ?>
